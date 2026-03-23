4e7916e0c5c97306765c8f43d9312f0f6d49d965
Bartosz Kozyra <bartosz.kozyraktos@outlook.com>
2026-03-23 13:16:41 +0100

    First Tests

--- main.c
+++ main.c
@@ -10,8 +10,8 @@
 
 typedef struct{
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE +1];
 } Row;
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
@@ -51,6 +51,8 @@ typedef struct{
 typedef enum{
     PREPARE_SUCCESS,
     PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR,
 } PrepareResult;
 
@@ -89,22 +91,47 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 
 }
 
-PrepareResult prepare_statement(InputBuffer* input_buffer,
-                                Statement* statement) {
-  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
     statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-    if (args_assigned < 3){
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if( keyword == NULL || id_string == NULL || username == NULL || email == NULL){
         return PREPARE_SYNTAX_ERROR;
     }
-    return PREPARE_SUCCESS;
-  }
-  if (strcmp(input_buffer->buffer, "select") == 0) {
-    statement->type = STATEMENT_SELECT;
-    return PREPARE_SUCCESS;
-  }
 
-  return PREPARE_UNRECOGNIZED_STATEMENT;
+    int id = atoi(id_string); // asci to integer
+    if (id < 0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+
+};
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        return prepare_insert(input_buffer, statement);
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
 void serialize_row(Row* source, void* destination){
@@ -215,8 +242,14 @@ int main(int argc, char* argv[]) {
         switch (prepare_statement(input_buffer, &statement)){
             case(PREPARE_SUCCESS):
                 break;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
             case(PREPARE_SYNTAX_ERROR):
                 printf("Syntax error. Could not parse statement.\n");
+                continue;
+            case(PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
                 continue;
             case(PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecpgnized keyword at start of '%s'.\n", input_buffer->buffer);
