b027952af16ff8832ea6a13d8061203486affc0c
Bartosz Kozyra <bartosz.kozyraktos@outlook.com>
2026-03-27 14:32:55 +0100

    B-Tree Leaf Node Format

--- test_db.py
+++ test_db.py
@@ -108,3 +108,41 @@ def test_keeps_data_after_closing_connection():
     ]
 
     assert expected2 == result2
+
+def test_prints_constants():
+    result = run_script([
+        ".constants",
+        ".exit",
+    ])
+
+    expected = [
+        "db > Constants:",
+        "ROW_SIZE: 293",
+        "COMMON_NODE_HEADER_SIZE: 6",
+        "LEAF_NODE_HEADER_SIZE: 10",
+        "LEAF_NODE_CELL_SIZE: 297",
+        "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+        "LEAF_NODE_MAX_CELLS: 13",
+        "db > "
+        ]
+    
+    assert expected == result
+
+
+def test_allows_printing_out_the_structure_of_a_one_node_btree():
+    script = ["insert #{i} user#{i} person#{i}@example.com" for i in [3,1,2]]
+
+    result = run_script(script)
+    expected = [
+        "db > Executed.",
+        "db > Executed.",
+        "db > Executed.",
+        "db > Tree:",
+        "leaf (size 3)",
+        "  - 0 : 3",
+        "  - 1 : 1",
+        "  - 2 : 2",
+        "db > "
+        ]
+    
+    assert expected == result
